43b589e4277e56bfb2e6a0113e33caf653a276f9
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2024-10-28 17:18:12 +0000

    project completed

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,20 +1,31 @@
 import prisma from "@/lib/db";
 import { unstable_cache as cache } from "next/cache";
 
-const getCachedPost = cache((slug) => {
-  return prisma.post.findUnique({
-    where: {
-      slug: slug,
-    },
-  });
-});
+// CACHE A POST
+// const getCachedPost = cache((slug) => {
+//   return prisma.post.findUnique({
+//     where: {
+//       slug: slug,
+//     },
+//   });
+// });
 
 export default async function PostPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const post = await getCachedPost(params.slug);
+  // const post = await getCachedPost(params.slug);    // CACHE A POST
+
+  const post = await prisma.post.findUnique({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      author: true,
+    },
+  });
+
   return (
     <main>
       <h1>{post?.title}</h1>
